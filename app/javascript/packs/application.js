75965994961a5adf60372015f4b817ec011fc4ff
Tenacious-Qi <joe.mccann@mailbox.org>
2021-02-26 11:18:42 -0500

    Add timeout function for flash messages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -11,3 +12,11 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+function hideNotice() {
+  setInterval(function() {
+    document.querySelector('.notification').style.display = "none";
+  }, 5000);
+}
+
+hideNotice();
